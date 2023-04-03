59a942b641e4611f20165a3f65128501064af2d5
mambaur <alwiroziq1998@gmail.com>
2023-04-04 00:39:14 +0700

    update config

--- lib/config.dart
+++ lib/config.dart
@@ -1,5 +1,4 @@
 class Config {
-  static int appId = 517110545;
-  static String appSign =
-      '46f7b428a2c0dfb06467092abec3dac36b8f7aaff1522ffc8c2ba13d1e221444';
+  static int appId = 0; // your app id
+  static String appSign = 'your app sign';
 }
